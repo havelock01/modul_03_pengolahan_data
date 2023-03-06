df2626762edd1499f939cd3bee445bc33118d3ee
Muhammad Rizqi <mrizqi0153@gmail.com>
2023-03-06 12:06:23 +0700

    Initial commit

---
+++ lib/login_page.dart
@@ -0,0 +1,91 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  String username = "";
+  String password = "";
+
+  Widget _usernameField() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        enabled: true,
+        onChanged: (value) {
+          username = value;
+        },
+        decoration: InputDecoration(
+          hintText: 'Username',
+          contentPadding: EdgeInsets.all(8.0),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            borderSide: BorderSide(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordField() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        enabled: true,
+        obscureText: true,
+        onChanged: (value) {
+          password = value;
+        },
+        decoration: InputDecoration(
+          hintText: 'Password',
+          contentPadding: EdgeInsets.all(8.0),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            borderSide: BorderSide(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      width: MediaQuery.of(context).size.width,
+      child: ElevatedButton(
+        onPressed: () {
+          String text = "";
+          if (username == "havelock" && password == "rizqi123") {
+            text = "Login Success";
+          } else {
+            text = "Login Failed";
+          }
+
+          SnackBar snackBar = SnackBar(content: Text(text));
+
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        child: Text('Login'),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Login Page"),
+        ),
+        body: Column(
+          children: [
+            _usernameField(),
+            _passwordField(),
+            _loginButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+}
